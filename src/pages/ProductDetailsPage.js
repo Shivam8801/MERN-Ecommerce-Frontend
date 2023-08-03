144d969850ed2e4403e7f86b910dbc197a7198c5
shivam8801 <shivammh15@gmail.com>
2023-08-03 17:02:10 +0530

    added checkout component, summary, detl form, ovrw

---
+++ src/pages/ProductDetailsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductDetails from '../features/product/components/ProductDetails'
+import Navbar from '../features/navbar/Navbar'
+
+export default function ProductDetailsPage() {
+    return (
+        <Navbar>
+            <ProductDetails></ProductDetails>
+        </Navbar>
+    )
+}
