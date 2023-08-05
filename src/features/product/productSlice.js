61b415469601b1084535e78e3ed12ccc9715a2b7
shivam8801 <shivammh15@gmail.com>
2023-08-05 20:19:34 +0530

    added br, cats from test.js, filter product logic

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts } from './productAPI';
+import { fetchAllProducts, fetchProductsByFilters } from './productAPI';
 
 const initialState = {
   products: [],
@@ -10,6 +10,14 @@ export const fetchAllProductsAsync = createAsyncThunk(
   'product/fetchAllProducts',
   async () => {
     const response = await fetchAllProducts();
+    return response.data;
+  } 
+);
+
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  'product/fetchProductsByFilters',
+  async (filter) => {
+    const response = await fetchProductsByFilters(filter);
     return response.data;
   }
 );
@@ -31,7 +39,16 @@ export const productSlice = createSlice({
       .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
         state.status = 'idle';
         state.products = action.payload;
-      });
+      })
+
+      // filtered products
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
+      })
   },
 });
 
@@ -39,6 +56,5 @@ export const { increment } = productSlice.actions;
 
 
 export const selectAllProducts = (state) => state.product.products;
-
 
 export default productSlice.reducer;
