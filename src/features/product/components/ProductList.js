10655c703f3867ceaf991dd54d3626ca728f0d83
shivam8801 <shivammh15@gmail.com>
2023-08-07 23:42:52 +0530

    added logic for pagination

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -1,12 +1,13 @@
 import React, { useState, Fragment, useEffect } from 'react';
-
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, FunnelIcon, MinusIcon, PlusIcon, Squares2X2Icon } from '@heroicons/react/20/solid'
 import { ChevronLeftIcon, ChevronRightIcon, StarIcon } from '@heroicons/react/20/solid'
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchAllProductsAsync, selectAllProducts, fetchProductsByFiltersAsync } from '../productSlice';
+import { fetchAllProductsAsync, selectAllProducts, fetchProductsByFiltersAsync, selectTotalItems } from '../productSlice';
+import { ITEMS_PER_PAGE } from '../../../app/constant';
+
 
 
 const sortOptions = [
@@ -14,6 +15,7 @@ const sortOptions = [
     { name: 'Price: Low to High', sort: 'price', order: 'asc', current: false },
     { name: 'Price: High to Low', sort: 'price', order: 'desc', current: false },
 ]
+
 const subCategories = [
     { name: 'Totes', href: '#' },
     { name: 'Backpacks', href: '#' },
@@ -243,9 +245,13 @@ export default function ProductList() {
     const dispatch = useDispatch()
     const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
     const products = useSelector(selectAllProducts)
+    const totalItems = useSelector(selectTotalItems)
 
     const [filter, setFilter] = useState({});
     const [sort, setSort] = useState({});
+    const [page, setPage] = useState(1);
+
+
 
     const handleFilter = (e, section, option) => {
         const newFilter = { ...filter }
@@ -277,10 +283,19 @@ export default function ProductList() {
         setSort(sort)
     }
 
+    const handlePage = (page) => {
+        setPage(page)
+    }
+
 
     useEffect(() => {
-        dispatch(fetchProductsByFiltersAsync({ filter, sort }))
-    }, [dispatch, filter, sort])
+        const pagination = { _page: page, _limit: ITEMS_PER_PAGE }
+        dispatch(fetchProductsByFiltersAsync({ filter, sort, pagination }))
+    }, [dispatch, filter, sort, page])
+
+    useEffect(()=>{
+        setPage(1)
+    },[totalItems, sort])
 
     return (
         <>
@@ -376,7 +391,7 @@ export default function ProductList() {
                                 {/* section of products and filters ends here */}
 
 
-                                <Pagination></Pagination>
+                                <Pagination page={page} setPage={setPage} handlePage={handlePage} totalItems={totalItems}></Pagination>
 
                             </main>
                         </div>
@@ -543,7 +558,7 @@ function DesktopFilter({ handleFilter }) {
     )
 }
 
-function Pagination() {
+function Pagination({ page, setPage, handlePage, totalItems }) {
     return (
         <div div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6" >
             <div className="flex flex-1 justify-between sm:hidden">
@@ -563,8 +578,8 @@ function Pagination() {
             <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
                 <div>
                     <p className="text-sm text-gray-700">
-                        Showing <span className="font-medium">1</span> to <span className="font-medium">10</span> of{' '}
-                        <span className="font-medium">97</span> results
+                        Showing <span className="font-medium">{(page - 1) * ITEMS_PER_PAGE + 1}</span> to <span className="font-medium">{(page) * ITEMS_PER_PAGE > totalItems ? totalItems : (page) * ITEMS_PER_PAGE}</span> of{' '}
+                        <span className="font-medium">{totalItems}</span> results
                     </p>
                 </div>
                 <div>
@@ -577,19 +592,21 @@ function Pagination() {
                             <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
                         </a>
                         {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-                        <a
-                            href="#"
-                            aria-current="page"
-                            className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                        >
-                            1
-                        </a>
-                        <a
-                            href="#"
-                            className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        >
-                            2
-                        </a>
+
+
+                        {
+                            Array.from({ length: Math.ceil(totalItems / ITEMS_PER_PAGE) }).map(
+                                (el, index) => (
+                                    <div
+                                        onClick={(e) => handlePage(index + 1)}
+                                        aria-current="page"
+                                        className={`relative cursor-pointer z-10 inline-flex items-center ${index + 1 === page ? 'bg-indigo-600 text-white' : 'text-gray-400'} px-4 py-2 text-sm font-semibold focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
+                                    >
+                                        {index + 1}
+                                    </div>
+                                ))
+                        }
+
 
                         <a
                             href="#"
